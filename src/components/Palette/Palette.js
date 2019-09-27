a2f43cc79af19b022f37be6e02c43e7cef8e66a0
Bartosz Ligęza <bligeza@tidk.pl>
2019-09-27 09:43:03 +0200

    added navbar component

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
 import './Palette.scss'
 import ColorBox from '../ColorBox/ColorBox'
+import Navbar from '../Navbar/Navbar'
 
 const Palette = props => {
   const [level, setLevel] = useState(500)
@@ -15,12 +14,10 @@ const Palette = props => {
   const colorBoxes = colors[level].map(color => (
     <ColorBox background={color.hex} name={color.name} />
   ))
+
   return (
     <div className="Palette">
-      <div className="slider">
-        <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
-      </div>
-      {/* navbar */}
+      <Navbar level={level} changeLevel={changeLevel} />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer */}
     </div>
