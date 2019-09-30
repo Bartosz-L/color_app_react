c545ae804e29bde5cbc240e51e12c3469cfba2be
Bartosz Ligęza <bligeza@tidk.pl>
2019-09-30 09:52:03 +0200

    routing for individual palette

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Palette from './components/Palette/Palette'
 import seedColors from './seedColors'
 import { generatePalette } from './utils/colorHelpers'
 
-function App() {
-  const palette = generatePalette(seedColors[4])
+const App = () => {
+  const findPalette = id => {
+    return seedColors.find(palette => {
+      return palette.id === id
+    })
+  }
 
   return (
-    <div>
-      <Palette palette={palette} />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <h1>palette list</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />
+          )}
+        />
+      </Switch>
+
+      {/* <Palette palette={palette} /> */}
+    </Router>
   )
 }
 
