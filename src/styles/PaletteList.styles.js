7963bddfcc9f4eabaddd5289495f1d5564331c28
Bartosz Ligęza <bligeza@tidk.pl>
2019-10-07 11:15:52 +0200

    palette list responsive grid

--- src/styles/PaletteList.styles.js
+++ src/styles/PaletteList.styles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes'
+
 export default {
   root: {
     backgroundColor: 'white',
@@ -12,6 +14,9 @@ export default {
     alignItems: 'flex-start',
     flexDirection: 'column',
     flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%',
+    },
   },
   nav: {
     display: 'flex',
@@ -29,6 +34,14 @@ export default {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%',
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
+      gridGap: '1.5rem',
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1rem',
+    },
   },
 }
