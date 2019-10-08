94b9e570b4c17e23048174a814be9ca646f7e363
Bartosz Ligęza <bligeza@tidk.pl>
2019-10-08 11:21:13 +0200

    build

---
+++ build/precache-manifest.a4a860b6fcb718150a03bf6dd2b1e950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1643de5ee33f8b7a95af7dd2a2bc01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a17e331c31fa25c8ee8",
+    "url": "/static/css/2.5774dd83.chunk.css"
+  },
+  {
+    "revision": "3305615d98cdaef5e212",
+    "url": "/static/css/main.2ab65873.chunk.css"
+  },
+  {
+    "revision": "6a17e331c31fa25c8ee8",
+    "url": "/static/js/2.9908f972.chunk.js"
+  },
+  {
+    "revision": "3305615d98cdaef5e212",
+    "url": "/static/js/main.26d2a29d.chunk.js"
+  },
+  {
+    "revision": "8733e0336d794a45549d",
+    "url": "/static/js/runtime-main.b8614ef4.js"
+  },
+  {
+    "revision": "53b0cdc9bd08aefe911fb151ed463030",
+    "url": "/static/media/Quantum-Gradient.53b0cdc9.svg"
+  }
+]);
